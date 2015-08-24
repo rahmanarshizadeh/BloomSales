4bb540c4ff139ab2a55c0d8b4e99d00ce7d0f5b3
Mohammad Riazat <moriazat@gmail.com>
2015-08-24 20:50:14 +0430

    Added TestHelper project to the solution

---
+++ Services/Source/BloomSales.TestHelpers/EntityMockFactory.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomSales.TestHelpers
+{
+    public static class EntityMockFactory
+    {
+        public static Mock<DbSet<TEntity>> CreateSet<TEntity>(IQueryable<TEntity> data) 
+            where TEntity : class
+        {
+            var mockSet = new Mock<DbSet<TEntity>>();
+            mockSet.As<IQueryable<TEntity>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<TEntity>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<TEntity>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<TEntity>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
---
+++ Services/Source/BloomSales.TestHelpers/Equality.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomSales.TestHelpers
+{
+    public static class Equality
+    {
+        public static bool AreEqual<T>(IEnumerable<T> expected, IEnumerable<T> actual)
+        {
+            if (expected == null && actual != null ||
+                expected != null && actual == null)
+                return false;
+
+            IEnumerator<T> it1 = expected.GetEnumerator();
+            IEnumerator<T> it2 = actual.GetEnumerator();
+
+            bool expectedMoved;
+            bool actualMoved;
+
+            while (true)
+            {
+                expectedMoved = it1.MoveNext();
+                actualMoved = it2.MoveNext();
+
+                if ((!expectedMoved && actualMoved) ||
+                     (expectedMoved && !actualMoved))
+                    return false;
+
+                if (!expectedMoved && !actualMoved)
+                    return true;
+
+                if (!it1.Current.Equals(it2.Current))
+                    return false;
+            }
+        }
+    }
+}
