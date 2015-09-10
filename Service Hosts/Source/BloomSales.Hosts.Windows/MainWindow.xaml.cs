b78c4db8bc97e08a0d2781bd58ced6f9af51dfc3
Mohammad Riazat <moriazat@gmail.com>
2015-09-10 05:28:31 +0430

    Added and implemented a desktop application for services control panel.

---
+++ Service Hosts/Source/BloomSales.Hosts.Windows/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using BloomSales.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BloomSales.Host.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ServiceHost hostLocationService;
+        private ServiceHost hostInventoryService;
+        private ServiceHost hostShippingService;
+        private ServiceHost hostAccountingService;
+        private ServiceHost hostOrderService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            path += @"\..\..\App Data";
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
+
+        private void StartLocationService()
+        {
+            this.hostLocationService = new ServiceHost(typeof(LocationService));
+            this.hostLocationService.Open();
+        }
+
+        private void StopLocationService()
+        {
+            this.hostLocationService.Close();
+        }
+
+        private void StartInventoryService()
+        {
+            this.hostInventoryService = new ServiceHost(typeof(InventoryService));
+            this.hostInventoryService.Open();
+        }
+
+        private void StopInventoryService()
+        {
+            this.hostInventoryService.Close();
+        }
+
+        private void StartShippingService()
+        {
+            this.hostShippingService = new ServiceHost(typeof(ShippingService));
+            this.hostShippingService.Open();
+        }
+
+        private void StopShippingService()
+        {
+            this.hostShippingService.Close();
+        }
+
+        private void StartAccountingService()
+        {
+            this.hostAccountingService = new ServiceHost(typeof(AccountingService));
+            this.hostAccountingService.Open();
+        }
+
+        private void StopAccountingService()
+        {
+            this.hostAccountingService.Close();
+        }
+
+        private void StartOrerService()
+        {
+            this.hostOrderService = new ServiceHost(typeof(OrderService));
+            this.hostOrderService.Open();
+        }
+
+        private void StopOrderService()
+        {
+            this.hostOrderService.Close();
+        }
+
+        private void Log(string log)
+        {
+            this.txtLog.Text += 
+                string.Format("[{0}]: {1}{2}", DateTime.Now.ToString(), log, Environment.NewLine);
+        }
+
+        private void btnLocation_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnLocation.Content.Equals("Start"))
+            {
+                StartLocationService();
+                btnLocation.Content = "Stop";
+                Log("Location service started.");
+            }
+            else if (btnLocation.Content.Equals("Stop"))
+            {
+                StopLocationService();
+                btnLocation.Content = "Start";
+                Log("Location service stopped.");
+            }
+        }
+
+        private void btnInventory_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnInventory.Content.Equals("Start"))
+            {
+                StartInventoryService();
+                btnInventory.Content = "Stop";
+                Log("Inventory service started.");
+            }
+            else if (btnInventory.Content.Equals("Stop"))
+            {
+                StopInventoryService();
+                btnInventory.Content = "Start";
+                Log("Inventory service stopped.");
+            }
+        }
+
+        private void btnAccounting_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnAccounting.Content.Equals("Start"))
+            {
+                StartAccountingService();
+                btnAccounting.Content = "Stop";
+                Log("Accounting service started.");
+            }
+            else if (btnAccounting.Content.Equals("Stop"))
+            {
+                StopAccountingService();
+                btnAccounting.Content = "Start";
+                Log("Accounting service stopped.");
+            }
+        }
+
+        private void btnShipping_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnShipping.Content.Equals("Start"))
+            {
+                StartShippingService();
+                btnShipping.Content = "Stop";
+                Log("Shipping service started.");
+            }
+            else if (btnShipping.Content.Equals("Stop"))
+            {
+                StopShippingService();
+                btnShipping.Content = "Start";
+                Log("Shipping service stopped.");
+            }
+        }
+
+        private void btnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnOrder.Content.Equals("Start"))
+            {
+                StartOrerService();
+                btnOrder.Content = "Stop";
+                Log("Order service started.");
+            }
+            else if (btnOrder.Content.Equals("Stop"))
+            {
+                StopOrderService();
+                btnOrder.Content = "Start";
+                Log("Order service stopped.");
+            }
+        }
+
+        private void btnAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnAll.Content.Equals("Start All"))
+                btnAll.Content = "Stop All";
+            else if (btnAll.Content.Equals("Stop All"))
+                btnAll.Content = "Start All";
+
+            btnLocation_Click(sender, e);
+            btnInventory_Click(sender, e);
+            btnShipping_Click(sender, e);
+            btnAccounting_Click(sender, e);
+            btnOrder_Click(sender, e);
+        }
+    }
+}
