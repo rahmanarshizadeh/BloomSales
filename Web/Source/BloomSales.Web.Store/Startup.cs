4e3213e513836455ec729a5e2338a94e190065b6
Mohammad Riazat <moriazat@gmail.com>
2016-06-19 14:08:31 -0400

    Added a general ASP.NET MVC project for BloomSales Online Store, along with a correlated unit test project.

---
+++ Web/Source/BloomSales.Web.Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BloomSales.Web.Store.Startup))]
+namespace BloomSales.Web.Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
