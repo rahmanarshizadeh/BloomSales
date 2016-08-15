8db7ef111d76bde4372eec9af41b582d605d56f8
Mohammad Riazat <moriazat@gmail.com>
2016-08-15 11:29:59 -0400

    Setting the "Store" homepage as the default route for the Website.

--- Web/Source/BloomSales.Web.Store/App_Start/RouteConfig.cs
+++ Web/Source/BloomSales.Web.Store/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BloomSales.Web.Store
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
